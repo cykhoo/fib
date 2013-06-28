8fb18b626177324ee345de5fbd83ecef66236b4e
Chong-Yee Khoo <mail@cykhoo.com>
2013-06-28 21:22:03 +0800

    add tests, implement function, add tests in filemaker

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,13 +1,18 @@
 #! usr/bin/ruby
 
 def fib(n)
+
+  if n.class != Fixnum
+    return 0
+  end
+
+  if n < 0
+    return 0
+  end
+
   if n < 3
     1
   else
     fib(n-1) + fib(n-2)
   end
 end
-
-# (1..30).each do |num|
-#   print fib(num).to_s + ", "
-# end
--- spec/fib_spec.rb
+++ spec/fib_spec.rb
@@ -1,2 +1,41 @@
 require 'rspec'
 require './lib/fib.rb'
+
+describe "fib function" do
+
+  it "should handle 1" do
+    fib(1).should == 1
+  end
+
+  it "should handle 2" do
+    fib(2).should == 1
+  end
+
+  it "should handle 3" do
+    fib(3).should == 2
+  end
+
+  it "should handle 4" do
+    fib(4).should == 3
+  end
+
+  it "should handle 5" do
+    fib(5).should == 5
+  end
+
+  it "should handle 6" do
+    fib(6).should == 8
+  end
+
+  it "should take only positive numbers" do
+    fib(-5).should == 0
+  end
+
+  it "should take only integers" do
+    fib(5.2).should == 0
+  end
+
+  it "should not take strings" do
+    fib("monkey").should == 0
+  end
+end
